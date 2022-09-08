a4572bf0bdd3a55959217739d391152477335fb9
joao-soares-2000 <joao.159.soares@gmail.com>
2022-09-08 17:04:57 -0300

    criação dos primeiros componentes

---
+++ src/app/Models/produto.ts
@@ -0,0 +1,6 @@
+export interface  Produto{
+  id?: String;
+  descricao: String;
+  unMedida: String;
+  ncm: number;
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { ListaProdutosComponent } from './lista-produtos/lista-produtos.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: ListaProdutosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
